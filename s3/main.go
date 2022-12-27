ca55bd7dd61f65bef41b551051846e13280a162f
Marcus Rodan <marcus.rodan@sinch.com>
2022-12-27 17:40:52 +0100

    Bootstrap

---
+++ s3/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	s3 "github.com/pulumi/pulumi-aws/sdk/v4/go/aws/s3"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func createInfra(ctx *pulumi.Context) (*s3.Bucket, error) {
+	// Create an S3 bucket
+	bucket, err := s3.NewBucket(ctx, "my-bucket", &s3.BucketArgs{
+		Tags: pulumi.StringMap{
+			"Name":    pulumi.String("my-bucket-diff"),
+			"SomeTag": pulumi.String("somevalue"),
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return bucket, nil
+}
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		bucket, err := createInfra(ctx)
+
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("bucketName", bucket.ID())
+		return nil
+	})
+}
